daf4c8fb0c77035692202b23042ed2a049c96e5c
gemstone-source <sophiajulius5@gmail.com>
2022-11-25 13:14:31 +0300

    Addind i3-gaps

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 sudo apt update 
 sudo apt install i3 -y && sudo apt install feh  -y && sudo apt install arandr -y && sudo apt install lxappearance -y
 sudo apt install nitrogen -y && sudo apt install thunar -y && sudo apt install rofi -y  && sudo apt install i3blocks -y
-sudo apt install gnome-terminal -y && sudo apt install compton -y && sudo apt install polybar
+sudo apt install gnome-terminal -y && sudo apt install compton -y && sudo apt install polybar && sudo apt install i3-gaps
 
 # Setting i3 config files
 mkdir ~/.config/i3
