a25cf03c8f55d12151dddd331ac87a50eebd2b1a
gemstone-source <sophiajulius5@gmail.com>
2023-04-01 23:54:36 +0300

    Replacing feh with variety

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 #install i3 
 sudo apt update 
-sudo apt install i3 -y && sudo apt install feh  -y && sudo apt install arandr -y && sudo apt install lxappearance -y
+sudo apt install i3 -y && sudo apt install variety  -y && sudo apt install arandr -y && sudo apt install lxappearance -y
 sudo apt install nitrogen -y && sudo apt install thunar -y && sudo apt install rofi -y  && sudo apt install i3blocks -y
 sudo apt install gnome-terminal -y && sudo apt install compton -y && sudo apt install polybar -y && sudo apt install i3-gaps -y
 
