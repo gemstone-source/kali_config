08575d4ab08b8229fa5d798f75870d06ea763620
gemstone-source <sophiajulius5@gmail.com>
2026-06-25 00:32:47 +0300

    Clear broken packages installation in i3

--- setup.sh
+++ setup.sh
@@ -2,9 +2,8 @@
 
 #install i3 
 sudo apt update -y
-sudo apt install i3 -y && sudo apt install variety  -y && sudo apt install arandr -y && sudo apt install lxappearance -y
-sudo apt install nitrogen -y && sudo apt install thunar -y && sudo apt install rofi -y  && sudo apt install i3blocks -y
-sudo apt install gnome-terminal -y && sudo apt install compton -y && sudo apt install polybar -y && sudo apt install i3-gaps -y
+sudo apt install i3 -y && sudo apt install variety  -y && sudo apt install arandr -y && sudo apt install lxappearance -y && sudo apt install thunar -y && sudo apt install rofi -y  && sudo apt install i3blocks -y && 
+sudo apt install gnome-terminal -y && sudo apt install compton -y && sudo apt install polybar -y
 
 # Setting i3 config files
 mkdir ~/.config/i3
@@ -16,7 +15,7 @@ mkdir ~/.config/polybar
 cp -r polybar ~/.config/
 
 # Setting oh-my-posh
-curl -s https://ohmyposh.dev/install.sh | sudo bash -s
+# curl -s https://ohmyposh.dev/install.sh | sudo bash -s
 
 # Setting gtk themes
 # touch ~/.gtkrc-2.0
@@ -40,7 +39,7 @@ cp alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
 
 # Rofi setup
 git clone --depth=1 https://github.com/adi1090x/rofi.git
-cd rofiN
+cd rofi
 chmod +x setup.sh
 ./setup.sh
 mv ~/.config/rofi/config.rasi  ~/.config/rofi/config
